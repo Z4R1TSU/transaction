51283a6dcc171b786c6fd29fbb259855da093239
Zari_Tsu <zaritsu030907@gmail.com>
2025-06-14 21:14:28 +0800

    chore: 修改后端端口

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000, // You can change the port if needed
+    port: 3000,
     proxy: {
-      // Proxy API requests to your backend server
-      // Adjust the target based on your backend's address
       '/api': {
-        target: 'http://localhost:8080', // Assuming your Spring Boot backend runs on 8080
+        target: 'http://localhost:8888',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '') // Remove /api prefix when forwarding
       }
