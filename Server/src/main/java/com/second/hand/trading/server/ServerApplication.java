febe2873e05f382217d43e8b1602d5d1a95cb5a9
xuzhelei <xuzhelei@bytedance.com>
2025-06-15 13:33:42 +0800

    feat(服务注册): 配置服务注册与发现功能
    - 修改应用名称为second-hand-trading-server
    - 添加Eureka客户端依赖并启用服务发现
    - 配置Zuul路由规则，将所有请求转发至后端服务
    - 允许敏感头信息通过以支持Cookie会话管理

--- Server/src/main/java/com/second/hand/trading/server/ServerApplication.java
+++ Server/src/main/java/com/second/hand/trading/server/ServerApplication.java
@@ -3,6 +3,7 @@ package com.second.hand.trading.server;
 import com.second.hand.trading.server.utils.OrderTaskHandler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * 安全问题：缺少密码加密传输和存储，缺少userid的cookie加密
@@ -10,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * 部分接口未做幂等
  */
 @SpringBootApplication()
+@EnableDiscoveryClient
 public class ServerApplication {
 
     public static void main(String[] args) {
