bbd93d20c44052e658d631da534aeef850fc85fd
xuzhelei <xuzhelei@bytedance.com>
2025-06-15 12:36:30 +0800

    refactor(zuul): 移除端口检查逻辑并简化启动方式
    将端口检查逻辑移除，改为在配置文件中直接设置端口
    使用SpringApplication.run简化启动方式

--- zuul/src/main/java/com/second/hand/trading/ServiceZuulApplication.java
+++ zuul/src/main/java/com/second/hand/trading/ServiceZuulApplication.java
@@ -1,8 +1,7 @@
 package com.second.hand.trading;
 
-import cn.hutool.core.util.NetUtil;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
@@ -13,12 +12,6 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 @EnableDiscoveryClient
 public class ServiceZuulApplication {
 	public static void main(String[] args) {
-    	int port = 8040;
-		if(!NetUtil.isUsableLocalPort(port)) {
-			System.err.printf("端口%d被占用了，无法启动%n", port );
-    		System.exit(1);
-    	}
-        new SpringApplicationBuilder(ServiceZuulApplication.class).properties("server.port=" + port).run(args);
-
+    	SpringApplication.run(ServiceZuulApplication.class, args);
 	}
 }
