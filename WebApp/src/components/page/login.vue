20488ec8c8d38672906b99034597c4f2f56bba84
Zari_Tsu <zaritsu030907@gmail.com>
2025-06-30 00:25:04 +0800

    feat: 去除占位信息

--- WebApp/src/components/page/login.vue
+++ WebApp/src/components/page/login.vue
@@ -34,8 +34,8 @@
         data() {
             return {
                 userForm: {
-                    accountNumber: '17322611234',
-                    userPassword: '123456'
+                    accountNumber: '',
+                    userPassword: ''
                 }
             };
         },
